49222585d9713ff9c78fc3b917837936ffaacd47
rclations <contact@rclations.com>
2017-07-12 11:36:55 -0400

    re-adding the rss feed rcp exclusion

--- wp-content/themes/thecrimereport/functions.php
+++ wp-content/themes/thecrimereport/functions.php
@@ -28,7 +28,7 @@ add_action( 'wp_enqueue_scripts', 'child_stylesheet', 20 );
  */
 require_once( dirname(__FILE__) . '/inc/rcp-term-restrictions-override.php' );
 require_once( dirname(__FILE__) . '/inc/rcp-user-mailchimp-signup-display.php' );
-// require_once( dirname(__FILE__) . '/inc/rcp-feed-override.php' );
+require_once( dirname(__FILE__) . '/inc/rcp-feed-override.php' );
 
 /**
  * extend crime report excerpt length to 80 words
